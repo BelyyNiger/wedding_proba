415946155d80d255bf88ce4c3e5d418ee2dde07f
BelyyNiger <smail89093@gmail.com>
2025-10-18 00:25:56 +0300

    Add files via upload

---
+++ dangers.js
@@ -0,0 +1,5 @@
+// dangers.js - Файл с секретными данными
+// ВНИМАНИЕ: Этот файл должен быть добавлен в .gitignore если используете Git
+
+const TELEGRAM_BOT_TOKEN = '8402820015:AAG6Fyt7zmM7ybRpqgyrAEVsnYPYkrpIPRg';
+const TELEGRAM_CHAT_ID = '1017940238';
